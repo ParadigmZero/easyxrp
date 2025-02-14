84228e19393aa1b0485e9ba7d1996634a13d1aae
ParadigmZero <josephrcalton@gmail.com>
2025-02-14 18:46:26 +0000

    Seperate functionality into seperate function, for use as external library for instance

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 #! /usr/bin/env node
 const { generateSeed, deriveKeypair, deriveAddress } = require("ripple-keypairs");
 
-const seed = generateSeed();
+function generateXRPwallet() {
+    const seed = generateSeed();
+    const keypair = deriveKeypair(seed);
+    return {
+        seed: seed,
+        walletAddress: `${deriveAddress(keypair.publicKey)}`,
+        privateKey: `${keypair.privateKey}`,
+    }
+};
 
-const keypair = deriveKeypair(seed);
+const wallet = generateXRPwallet();
 
 console.log(
-`
-XRP wallet will not be activated until the minimum reserve or XRP is sent to to that wallet address. Check the official Ripple docs (e.g. https://xrpl.org/docs/concepts/accounts/reserves) as to how much that is.
-
-It is essential you note down the wallet address and private key. The private key must be kept secret.
-
-You may also note down the seed. The seed can be used to derive the wallet and private key, and so must also be kept secret. It can be used with various (trusted) applications, to add this specific wallet.
-
-seed (keep secret):
-${seed}\n
-wallet address (public):
-${deriveAddress(keypair.publicKey)}\n
-private key (keep secret):
-${keypair.privateKey}`
+    `XRP wallet will not be activated until the minimum reserve or XRP is sent to to that wallet address.\n\nCheck the official Ripple docs (e.g. https://xrpl.org/docs/concepts/accounts/reserves) as to how much that is\n\nIt is essential you note down the wallet address and private key. The private key must be kept secret.\n\nYou may also note down the seed.\n\nThe seed can be used to derive the wallet and private key, and so must also be kept secret. It can be used with various (trusted) applications, to add this specific wallet.\n\nseed (keep secret):\n${wallet.seed}\n\nwallet address (public):\n${wallet.walletAddress}\n\nprivate key (keep secret):\n${wallet.privateKey}`
 );
